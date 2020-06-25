a9746d1effec9d981ceecda0f68ee84d3dc8d4b1
Rassoul Edji <65983679+RassoulEdji@users.noreply.github.com>
2020-06-25 15:34:21 +0100

    Update batchRenamer.py
    Batch Renamer - Rename and Search and Replace working

--- batchRenamer.py
+++ batchRenamer.py
@@ -1,147 +1,218 @@
+####################################
+# Maya Batch Renamer by Rassoul Edji
+####################################
+
 from maya import cmds
 import maya.api.OpenMaya as om
-import maya.OpenMayaUI as omui
 
-from PySide2 import QtCore
-from PySide2 import QtWidgets
-from shiboken2 import wrapInstance
+SUFFIXES = {
+    "mesh": "GEO",
+    "joint": "JNT",
+    "camera": None,
+    "ambientLight": "LGT"
+}
 
-############################
-# UI
-############################
+DEFAULT_SUFFIX = "GRP"
 
-def maya_main_window():
-    main_window_ptr = omui.MQtUtil.mainWindow()
-    return wrapInstance(long(main_window_ptr), QtWidgets.QWidget)
+class batchRenamerUi(object):
 
-class TestDialog(QtWidgets.QDialog):
+    WINDOW_NAME = "batchRenamerUi"
+    APP_NAME = "Batch Renamer"
     
-    def __init__(self, parent=maya_main_window()):
-        super(TestDialog, self).__init__(parent)
-        
-        self.setWindowTitle("Batch Renamer")
-        self.setMinimumWidth(650)
-        
-        self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
-        
-        self.create_widgets()
-        self.create_layouts()
-        self.create_connections()
-        
-    def create_widgets(self):
-        self.name_le = QtWidgets.QLineEdit()
-        
-        self.prefix_le = QtWidgets.QLineEdit()
-        self.suffix_le = QtWidgets.QLineEdit()
-        self.autoSuffix_cb = QtWidgets.QCheckBox("Auto Suffix")
-        self.padding_sb = QtWidgets.QSpinBox()
-        self.padding_sb.setFixedWidth(80)
-        self.padding_sb.setMinimum(1)
-        self.padding_sb.setMaximum(6)
-        
-        self.rename_btn = QtWidgets.QPushButton("Rename")
-        self.replace_btn = QtWidgets.QPushButton("Replace")
-        self.cancel_btn = QtWidgets.QPushButton("Cancel")
-        
-        self.counter = QtWidgets.QLabel(displayedAmount + " objects selected for renaming")
-        
-        self.selected_rb = QtWidgets.QRadioButton("Selected")
-        self.selected_rb.setChecked(True)
-        self.hierarchy_rb = QtWidgets.QRadioButton("Hierarchy")
-        
-        self.searchFor_le = QtWidgets.QLineEdit()
-        self.replaceWith_le = QtWidgets.QLineEdit()
+    @classmethod
+    def display(cls):
+        cls.delete()
 
-        
-    def create_layouts(self):
-        radio_btn_layout = QtWidgets.QHBoxLayout()
-        radio_btn_layout.addWidget(self.selected_rb)
-        radio_btn_layout.addWidget(self.hierarchy_rb)
-        
-        options_layout = QtWidgets.QHBoxLayout()
-        options_layout.addWidget(self.prefix_le)
-        options_layout.addWidget(self.suffix_le)
-        options_layout.addWidget(self.autoSuffix_cb)
-        options_layout.addWidget(self.padding_sb)
+        main_window = cmds.window(cls.WINDOW_NAME,
+                                  title="Batch Renamer",
+                                  sizeable=True,
+                                  menuBar=True)
 
-        button_layout = QtWidgets.QHBoxLayout()
-        button_layout.addWidget(self.rename_btn)
-        button_layout.addWidget(self.replace_btn)
-        button_layout.addWidget(self.cancel_btn)
+        main_layout = cmds.columnLayout(adjustableColumn=True, parent=main_window)
+
+        # Method section
+        method_layout = cmds.formLayout(parent=main_layout)
+
+        cls.method = cmds.radioButtonGrp(numberOfRadioButtons=2,
+                                            label="Method: ",
+                                            columnWidth=(1, 75),
+                                            h=(27),
+                                            w=512,
+                                            sl=1,
+                                            labelArray2=("Selected", "Hierarchy"),
+                                            parent=method_layout)
+
+        cmds.formLayout(method_layout, e=True, af=(cls.method, "top", 0))
+
+        # Rename Section
+        rename_layout = cmds.frameLayout(label="Rename", parent=main_layout, bgs=True)
+        rename_form_layout = cmds.formLayout(parent=rename_layout, h=60)
         
-        form_layout = QtWidgets.QFormLayout()
-        form_layout.addRow("Method:", radio_btn_layout)
-        form_layout.addRow("", self.counter)
-        form_layout.addRow("Name:", self.name_le)
-        form_layout.addRow("Prefix:", options_layout)
-        form_layout.addRow("Search for:", self.searchFor_le) 
-        form_layout.addRow("Replace with:", self.replaceWith_le) 
-        
-        main_layout = QtWidgets.QVBoxLayout(self)
-        main_layout.addLayout(form_layout)
-        main_layout.addLayout(button_layout)
-        
-    def create_connections(self):
-        self.name_le.editingFinished.connect(self.name_data)
-        
-        self.autoSuffix_cb.toggled.connect(self.auto_suffix_data)
-        
-        self.cancel_btn.clicked.connect(self.close)
-        
-    def name_data(self):
-        name = self.name_le.text()
-        print("Hello {0}!".format(name))
-        
-    def auto_suffix_data(self):
-        auto_suffix = self.autoSuffix_cb.isChecked()
-        if auto_suffix:
-            print("Auto Suffix Turned On")
+        cls.name_tfg = cmds.textFieldGrp(label="Name: ",                                               
+                                                columnWidth=(1, 75),
+                                                columnWidth2=(2, 426),
+                                                editable=True,
+                                                parent=rename_form_layout)
+        cls.prefix_tfg = cmds.textFieldGrp(label="Prefix: ",
+                                                columnWidth=(1, 75),
+                                                columnWidth2=(2, 100),
+                                                editable=True,
+                                                parent=rename_form_layout)
+        cls.suffix_tfg = cmds.textFieldGrp(label="Suffix: ",
+                                                columnWidth=(1, 35),
+                                                columnWidth2=(2, 100),
+                                                editable=True,
+                                                parent=rename_form_layout)
+        cls.autoSuffix_cbg = cmds.checkBoxGrp(numberOfCheckBoxes=1,
+                                               label="Auto Suffix: ",
+                                               columnWidth=(1, 60),
+                                               io=False,
+                                               parent=rename_form_layout)
+        padding_label = cmds.text("Padding: ", align="right", width=50, parent=rename_form_layout)
+        cls.padding = cmds.intField(width=50,
+                                            value=1,
+                                            minValue=1,
+                                            maxValue=6,
+                                            step=1,
+                                            parent=rename_form_layout)
+
+        cmds.formLayout(rename_form_layout, e=True, af=(cls.name_tfg, "top", 6))
+        cmds.formLayout(rename_form_layout, e=True, af=(cls.name_tfg, "left", 0))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.prefix_tfg, "top", 3, cls.name_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.suffix_tfg, "top", 3, cls.name_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.suffix_tfg, "left", 0, cls.prefix_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.autoSuffix_cbg, "top", 6, cls.name_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.autoSuffix_cbg, "left", 0, cls.suffix_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(padding_label, "top", 6, cls.name_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(padding_label, "left", 0, cls.autoSuffix_cbg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.padding, "top", 3, cls.name_tfg))
+        cmds.formLayout(rename_form_layout, e=True, ac=(cls.padding, "left", 0, padding_label))
+
+        # Search and Replace Section
+        replace_layout = cmds.frameLayout(label="Search and Replace", parent=main_layout, bgs=True)
+
+        replace_form_layout = cmds.formLayout(parent=replace_layout, h=65)
+
+        cls.searchFor_tfg = cmds.textFieldGrp(label="Search For: ",
+                                                columnWidth=(1, 75),
+                                                columnWidth2=(2, 426),
+                                                editable=True,
+                                                parent=replace_form_layout)
+        cls.replaceWith_tfg = cmds.textFieldGrp(label="Replace With: ",
+                                                columnWidth=(1, 75),
+                                                columnWidth2=(2, 426),
+                                                editable=True,
+                                                parent=replace_form_layout)
+
+        cmds.formLayout(replace_form_layout, e=True, af=(cls.searchFor_tfg, "top", 6))
+        cmds.formLayout(replace_form_layout, e=True, af=(cls.searchFor_tfg, "left", 0))
+        cmds.formLayout(replace_form_layout, e=True, ac=(cls.replaceWith_tfg, "top", 3, cls.searchFor_tfg))
+
+        # Buttons section
+        button_layout = cmds.flowLayout(parent=main_layout)
+        rename_btn = cmds.button(label="Rename",
+                                 width=170,
+                                 height=28,
+                                 command= "batchRenamerUi.rename_objects()",
+                                 parent=button_layout)
+        replace_btn = cmds.button(label="Replace",
+                                 width=170,
+                                 height=28,
+                                 command= "batchRenamerUi.replace_objects()",
+                                 parent=button_layout)
+        replace_btn = cmds.button(label="Cancel",
+                                 width=170,
+                                 height=28,
+                                 command="batchRenamerUi.delete()",
+                                 parent=button_layout)
+
+        cmds.window(main_window, e=True, w=200, h=150)
+        cmds.window(main_window, e=True, sizeable=True)
+        cmds.window(main_window, e=True, rtf=True)
+
+        cmds.showWindow(main_window)
+
+    @classmethod
+    def delete(cls):
+        if cmds.window(cls.WINDOW_NAME, exists=True):
+            cmds.deleteUI(cls.WINDOW_NAME, window=True)
+            
+    # This function renames the selected objects using the given values
+    @classmethod
+    def rename_objects(cls):
+
+        rename_data = cmds.textFieldGrp(cls.name_tfg, query=True, text=True)
+        prefix_data = cmds.textFieldGrp(cls.prefix_tfg, query=True, text=True)
+        padding_data = cmds.intField(cls.padding, query=True, value=True)
+        hierarchy_data = cmds.radioButtonGrp(cls.method, query=True, select=True)
+
+        # Selection method 
+        if hierarchy_data == 2:
+            sel = cmds.ls(sl=True, tr=True)
+            children = cmds.listRelatives(sel, ad=True, type='transform')
+            cmds.select(children, add=True)
+            sel = cmds.ls(sl=True)
         else: 
-            print("Auto Suffix Not On")
+            sel = cmds.ls(sl=True)
+
+        # Suffix calculation
+        if cmds.checkBoxGrp(cls.autoSuffix_cbg, query=True, value1=True):
+
+            for obj in sel:
+    
+                sel.sort(key=len, reverse=True)
+    
+                shortName = obj.split("|")[-1]
+
+                children = cmds.listRelatives(obj, children=True, fullPath=True) or []
+                print(children)
+                if len(children) == 1:
+                    child = children[0]
+                    objType = cmds.objectType(child)
         
+                    suffix_data = SUFFIXES.get(objType, DEFAULT_SUFFIX)
+        else: 
+            suffix_data = cmds.textFieldGrp(cls.suffix_tfg, query=True, text=True)
 
+        counter = 1
+        if len(sel) < 1:
+            raise RuntimeError("Please select at least one object to rename")
+        else:
+            for obj in sel:
+                cmds.rename("{0}{1}{2}{3}".format(prefix_data, rename_data, str(counter).zfill(padding_data), suffix_data))
+                counter += 1
+
+        print("Renamed {0} Objects".format(counter))
+        cmds.select(all=True, hierarchy = True, visible = False, deselect=True)
         
+    # This function searches for a given string and replaces it with another given string
+    @classmethod
+    def replace_objects(cls):
 
-#############################
-# Backend
-#############################
-
-# User inputs
-sel = cmds.ls(sl=True)
-
-
-# Displays how many objects are selected with comma seperators
-displayedAmount = (format(len(sel), ","))
-
-
-
-
-# Makes sure user has selected objects for renaming
-if not sel:
-    om.MGlobal.displayError("Please select objects to rename")
+        searchFor_data = cmds.textFieldGrp(cls.searchFor_tfg, query=True, text=True)
+        replaceWith_data = cmds.textFieldGrp(cls.replaceWith_tfg, query=True, text=True)
+        hierarchy_data = cmds.radioButtonGrp(cls.method, query=True, select=True)
         
+        if hierarchy_data == 2:
+            sel = cmds.ls(sl=True, tr=True)
+            children = cmds.listRelatives(sel, ad=True, type='transform')
+            cmds.select(children, add=True)
+            sel = cmds.ls(sl=True)
+        else: 
+            sel = cmds.ls(sl=True)
 
-def rename_objects(name, startsAt=1):
-    # This function renames the selected objects
-    counter = startsAt
-    for i in sel:
-        cmds.rename('%s_%s' % (name, counter))
-        counter+=1
-        
-#def searchAndReplace():
-    # This function searches the selected objects for a given string and replaces it with the user inputted string
-    # counter = startsAt
-    
-if __name__ == '__main__':
-    
-    try:
-        test_dialog.close() # pylint: disable=E0601
-        test_dialog.deleteLater()
-    except:
-        pass
-    
-    test_dialog = TestDialog()
-    test_dialog.show()
-    
-    
+        if len(sel) < 1:
+            raise RuntimeError("Please select at least one object to replace")
+        elif searchFor_data == "":
+            raise RuntimeError("Please enter a name to search for")
+        elif replaceWith_data == "":
+            raise RuntimeError("Please enter a name to replace with")
+        else:
+            for i in sel:
+                if searchFor_data in i:
+                    renamedString = i.replace(searchFor_data, replaceWith_data)
+                    cmds.rename(i, renamedString)
 
+if __name__ == "__main__":
+    
+    batchRenamerUi.display()
